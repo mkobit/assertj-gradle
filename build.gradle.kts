5bc41d9945bd2a5a5d9d9b23fc1753397cda2d14
Mike Kobit <mkobit@gmail.com>
2017-09-22 11:30:03 -0500

    Incorporate changes to bring build working and incorporate changes from split repository
    Includes most changes up until https://github.com/mkobit/gradle-test-kotlin-extensions/commit/9a3363512e128e1c46239acd78b61c24a4273add

---
+++ build.gradle.kts
@@ -0,0 +1,127 @@
+import org.gradle.api.internal.HasConvention
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.junit.platform.console.options.Details
+import org.junit.platform.gradle.plugin.JUnitPlatformExtension
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+
+buildscript {
+  repositories {
+    mavenCentral()
+    jcenter()
+  }
+  dependencies {
+    // TODO: load from properties or script plugin
+    classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.0")
+    classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.15")
+  }
+}
+
+plugins {
+  `java-library`
+  `maven-publish`
+  kotlin("jvm")
+  id("com.github.ben-manes.versions") version "0.15.0"
+}
+
+val SourceSet.kotlin: SourceDirectorySet
+  get() = (this as HasConvention).convention.getPlugin(KotlinSourceSet::class.java).kotlin
+
+tasks {
+  "wrapper"(Wrapper::class) {
+    gradleVersion = "4.2"
+  }
+}
+
+version = "0.1.0"
+group = "com.mkobit.gradle.test"
+repositories {
+  jcenter()
+  mavenCentral()
+}
+
+apply {
+  from("gradle/junit5.gradle.kts")
+  plugin("org.junit.platform.gradle.plugin")
+  plugin("org.jetbrains.dokka")
+}
+
+val kotlinVersion by project
+val junitPlatformVersion: String by rootProject.extra
+val junitTestImplementationArtifacts: Map<String, Map<String, String>> by rootProject.extra
+val junitTestRuntimeOnlyArtifacts: Map<String, Map<String, String>> by rootProject.extra
+
+dependencies {
+  api(gradleApi())
+  api(gradleTestKit())
+  testImplementation(kotlin("reflect", kotlinVersion as String))
+  testImplementation("org.assertj:assertj-core:3.8.0")
+  testImplementation("org.mockito:mockito-core:2.10.0")
+  testImplementation("com.nhaarman:mockito-kotlin:1.5.0")
+  junitTestImplementationArtifacts.values.forEach {
+    testImplementation(it)
+  }
+  junitTestRuntimeOnlyArtifacts.values.forEach {
+    testRuntimeOnly(it)
+  }
+  testImplementation(kotlin("stdlib-jre8", kotlinVersion as String))
+}
+
+extensions.getByType(JUnitPlatformExtension::class.java).apply {
+  platformVersion = junitPlatformVersion
+  filters {
+    engines {
+      include("junit-jupiter")
+    }
+  }
+  logManager = "org.apache.logging.log4j.jul.LogManager"
+  details = Details.TREE
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+val main = java.sourceSets["main"]!!
+// No Kotlin in main source set
+main.kotlin.setSrcDirs(emptyList<Any>())
+
+val sourcesJar by tasks.creating(Jar::class) {
+  classifier = "sources"
+  from(main.allSource)
+  description = "Assembles a JAR of the source code"
+  group = JavaBasePlugin.DOCUMENTATION_GROUP
+}
+
+val javadocJar by tasks.creating(Jar::class) {
+  classifier = "javadoc"
+  val javadoc by tasks.getting(Javadoc::class)
+  dependsOn(javadoc)
+  from(javadoc.destinationDir)
+  description = "Assembles a JAR of the generated Javadoc"
+  group = JavaBasePlugin.DOCUMENTATION_GROUP
+}
+
+tasks["assemble"].dependsOn(sourcesJar, javadocJar)
+
+publishing {
+  publications.invoke {
+    "library"(MavenPublication::class) {
+      from(components["java"])
+      artifact(sourcesJar)
+      artifact(javadocJar)
+    }
+  }
+}
+
+tasks.withType(KotlinCompile::class.java) {
+  kotlinOptions.jvmTarget = "1.8"
+}
+
+dependencies {
+  compileOnly("com.google.code.findbugs:jsr305:3.0.2")
+  api("org.assertj", "assertj-core", "3.8.0")
+  testImplementation(kotlin("stdlib-jre8", kotlinVersion as String))
+}
