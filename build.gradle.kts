69452c9176f28ca50cd25bd7c4ced6f836767d53
Mike Kobit <mkobit@gmail.com>
2017-09-28 13:36:42 -0500

    Add in MIT license and prepare buildscript for Bintray publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+import java.io.ByteArrayOutputStream
 import org.gradle.api.internal.HasConvention
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.junit.platform.console.options.Details
@@ -21,19 +23,47 @@ plugins {
   `maven-publish`
   kotlin("jvm")
   id("com.github.ben-manes.versions") version "0.15.0"
-}
-
-val SourceSet.kotlin: SourceDirectorySet
-  get() = (this as HasConvention).convention.getPlugin(KotlinSourceSet::class.java).kotlin
-
-tasks {
-  "wrapper"(Wrapper::class) {
-    gradleVersion = "4.2"
-  }
+  id("com.jfrog.bintray") version "1.7.3"
 }
 
 version = "0.1.0"
 group = "com.mkobit.gradle.test"
+description = "AssertJ extensions for Gradle"
+
+val projectUrl by extra { "https://github.com/mkobit/gradle-test-kotlin-extensions" }
+val issuesUrl by extra { "https://github.com/mkobit/gradle-test-kotlin-extensions/issues" }
+val scmUrl by extra { "https://github.com/mkobit/gradle-test-kotlin-extensions.git" }
+
+val gitCommitSha: String by lazy {
+  ByteArrayOutputStream().use {
+    project.exec {
+      commandLine("git", "rev-parse", "HEAD")
+      standardOutput = it
+    }
+    it.toString(Charsets.UTF_8.name()).trim()
+  }
+}
+
+tasks.withType(Jar::class.java) {
+  manifest {
+    attributes(mapOf(
+      "Build-Revision" to gitCommitSha,
+      "Implementation-Version" to project.version
+      // TODO: include Gradle version?
+    ))
+  }
+}
+
+val SourceSet.kotlin: SourceDirectorySet
+  get() = withConvention(KotlinSourceSet::class) { kotlin }
+
+tasks {
+  "wrapper"(Wrapper::class) {
+    gradleVersion = "4.2"
+    distributionType = Wrapper.DistributionType.ALL
+  }
+}
+
 repositories {
   jcenter()
   mavenCentral()
@@ -56,7 +86,6 @@ dependencies {
   api("org.assertj", "assertj-core", "3.8.0")
   testImplementation(kotlin("stdlib-jre8", kotlinVersion as String))
   testImplementation(kotlin("reflect", kotlinVersion as String))
-  testImplementation("org.assertj:assertj-core:3.8.0")
   testImplementation("org.mockito:mockito-core:2.10.0")
   testImplementation("com.nhaarman:mockito-kotlin:1.5.0")
   junitTestImplementationArtifacts.values.forEach {
@@ -65,7 +94,6 @@ dependencies {
   junitTestRuntimeOnlyArtifacts.values.forEach {
     testRuntimeOnly(it)
   }
-  testImplementation(kotlin("stdlib-jre8", kotlinVersion as String))
 }
 
 extensions.getByType(JUnitPlatformExtension::class.java).apply {
@@ -96,26 +124,110 @@ val sourcesJar by tasks.creating(Jar::class) {
 }
 
 val javadocJar by tasks.creating(Jar::class) {
-  classifier = "javadoc"
   val javadoc by tasks.getting(Javadoc::class)
   dependsOn(javadoc)
   from(javadoc.destinationDir)
+  classifier = "javadoc"
   description = "Assembles a JAR of the generated Javadoc"
   group = JavaBasePlugin.DOCUMENTATION_GROUP
 }
 
-tasks["assemble"].dependsOn(sourcesJar, javadocJar)
+tasks {
+  withType(Jar::class.java) {
+    from(project.projectDir) {
+      include("LICENSE.txt")
+      into("META-INF")
+    }
+  }
 
+  withType(KotlinCompile::class.java) {
+    kotlinOptions.jvmTarget = "1.8"
+  }
+
+  "assemble" {
+    dependsOn(sourcesJar, javadocJar)
+  }
+
+  val gitDirtyCheck by creating {
+    doFirst {
+      val output = ByteArrayOutputStream().use {
+        exec {
+          commandLine("git", "status", "--porcelain")
+          standardOutput = it
+        }
+        it.toString(Charsets.UTF_8.name()).trim()
+      }
+      if (output.isNotBlank()) {
+        throw GradleException("Workspace is dirty:\n$output")
+      }
+    }
+  }
+
+  val gitTag by creating(Exec::class) {
+    description = "Tags the local repository with version ${project.version}"
+    group = PublishingPlugin.PUBLISH_TASK_GROUP
+    commandLine("git", "tag", "-a", project.version, "-m", "Gradle created tag for ${project.version}")
+  }
+
+  val pushGitTag by creating(Exec::class) {
+    description = "Pushes Git tag ${project.version} to origin"
+    group = PublishingPlugin.PUBLISH_TASK_GROUP
+    dependsOn(gitTag)
+    commandLine("git", "push", "origin", "refs/tags/${project.version}")
+  }
+
+  val bintrayUpload by getting {
+    dependsOn(gitDirtyCheck, gitTag)
+  }
+
+  "release" {
+    group = PublishingPlugin.PUBLISH_TASK_GROUP
+    description = "Publishes the library and pushes up a Git tag for the current commit"
+    dependsOn(bintrayUpload, pushGitTag)
+  }
+}
+
+val publicationName = "assertjGradle"
 publishing {
   publications.invoke {
-    "library"(MavenPublication::class) {
+    publicationName(MavenPublication::class) {
       from(components["java"])
       artifact(sourcesJar)
       artifact(javadocJar)
+      pom.withXml {
+        asNode().apply {
+          appendNode("description", project.description)
+          appendNode("url", projectUrl)
+          appendNode("licenses").apply {
+            appendNode("license").apply {
+              appendNode("name", "The MIT License")
+              appendNode("url", "https://opensource.org/licenses/MIT")
+              appendNode("distribution", "repo")
+            }
+          }
+        }
+      }
     }
   }
 }
 
-tasks.withType(KotlinCompile::class.java) {
-  kotlinOptions.jvmTarget = "1.8"
+bintray {
+  val bintrayUser = project.findProperty("bintrayUser") as String?
+  val bintrayApiKey = project.findProperty("bintrayApiKey") as String?
+  user = bintrayUser
+  key = bintrayApiKey
+  publish = true
+  setPublications(publicationName)
+  pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+    repo = "gradle"
+    name = project.name
+    userOrg = "mkobit"
+
+    setLabels("gradle", "testkit", "kotlin")
+    desc = project.description
+    websiteUrl = projectUrl
+    issueTrackerUrl = issuesUrl
+    vcsUrl = scmUrl
+  })
 }
+
