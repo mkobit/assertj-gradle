7329c775d7260c461fc96f073c6d113d73fc64c9
Mike Kobit <mkobit@gmail.com>
2018-11-27 08:50:43 -0600

    Set up basic assertions using `strikt` library

---
+++ strikt-gradle/strikt-gradle.gradle.kts
@@ -0,0 +1,120 @@
+import buildsrc.DependencyInfo
+import buildsrc.ProjectInfo
+import com.jfrog.bintray.gradle.BintrayExtension
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.dokka.DokkaConfiguration
+import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import java.net.URL
+
+plugins {
+  `java-library`
+  `maven-publish`
+  kotlin("jvm")
+  id("com.jfrog.bintray")
+  id("org.jetbrains.dokka")
+}
+
+description = "Strikt (https://github.com/robfletcher/strikt) extensions for Gradle"
+
+val SourceSet.kotlin: SourceDirectorySet
+  get() = withConvention(KotlinSourceSet::class) { kotlin }
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+  withType<Jar> {
+    manifest {
+      attributes(mapOf(
+        "Automatic-Module-Name" to "com.mkobit.gradle.test.strikt"
+      ))
+    }
+  }
+
+  val main by sourceSets
+  // No Java code, so don't need the javadoc task.
+  // Dokka generates our documentation.
+  remove(getByName("javadoc"))
+  val dokka by getting(DokkaTask::class) {
+    dependsOn(main.classesTaskName)
+    jdkVersion = 8
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
+    sourceDirs = main.kotlin.srcDirs
+    // See https://github.com/Kotlin/dokka/issues/196
+    externalDocumentationLink(delegateClosureOf<DokkaConfiguration.ExternalDocumentationLink.Builder> {
+      url = URL("https://docs.gradle.org/current/javadoc/")
+    })
+  }
+
+  val javadocJar by creating(Jar::class) {
+    dependsOn(dokka)
+    description = "Assembles a JAR of the generated Javadoc"
+    from(dokka.outputDirectory)
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    classifier = "javadoc"
+  }
+
+  val sourcesJar by creating(Jar::class) {
+    classifier = "sources"
+    from(main.allSource)
+    description = "Assembles a JAR of the source code"
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+  }
+
+  "assemble" {
+    dependsOn(javadocJar, sourcesJar)
+  }
+}
+
+dependencies {
+  api(DependencyInfo.strikt)
+}
+
+val publicationName = "striktGradle"
+publishing {
+  publications {
+    val sourcesJar by tasks.getting
+    val javadocJar by tasks.getting
+    register(publicationName, MavenPublication::class) {
+      from(components["java"])
+      artifact(sourcesJar)
+      artifact(javadocJar)
+      pom {
+        description.set(project.description)
+        url.set(ProjectInfo.projectUrl)
+        licenses {
+          license {
+            name.set("The MIT License")
+            url.set("https://opensource.org/licenses/MIT")
+            distribution.set("repo")
+          }
+        }
+      }
+    }
+  }
+}
+
+bintray {
+  val bintrayUser = project.findProperty("bintrayUser") as String?
+  val bintrayApiKey = project.findProperty("bintrayApiKey") as String?
+  user = bintrayUser
+  key = bintrayApiKey
+  publish = true
+  setPublications(publicationName)
+  pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+    repo = "gradle"
+    name = project.name
+    userOrg = "mkobit"
+
+    setLabels("gradle", "strikt", "assertion", "testkit")
+    setLicenses("MIT")
+    desc = project.description
+    websiteUrl = ProjectInfo.projectUrl
+    issueTrackerUrl = ProjectInfo.issuesUrl
+    vcsUrl = ProjectInfo.scmUrl
+  })
+}
