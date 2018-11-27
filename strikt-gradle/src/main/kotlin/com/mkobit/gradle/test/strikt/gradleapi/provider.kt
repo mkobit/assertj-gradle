c7745860929d673b84f2f8960c58c974a35916d3
Mike Kobit <mkobit@gmail.com>
2018-11-27 10:47:50 -0600

    Add in suppresion of unstable API usage

--- strikt-gradle/src/main/kotlin/com/mkobit/gradle/test/strikt/gradleapi/provider.kt
+++ strikt-gradle/src/main/kotlin/com/mkobit/gradle/test/strikt/gradleapi/provider.kt
@@ -3,8 +3,10 @@ package com.mkobit.gradle.test.strikt.gradleapi
 import org.gradle.api.provider.Provider
 import strikt.api.Assertion
 
+@Suppress("UnstableApiUsage")
 val <T : Provider<V>, V> Assertion.Builder<T>.isPresent: Assertion.Builder<Boolean>
   get() = get("is present") { isPresent }
 
+@Suppress("UnstableApiUsage")
 val <T : Provider<V>, V> Assertion.Builder<T>.value: Assertion.Builder<V?>
   get() = get("value") { orNull }
