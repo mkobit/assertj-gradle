c3564c471ab26acf9554f05ce5f12386e6dc90a6
Mike Kobit <mkobit@gmail.com>
2018-11-27 09:48:26 -0600

    Fix variance of extensions methods for assertk

--- assertk-gradle/src/main/kotlin/com/mkobit/gradle/test/assertk/gradleapi/task.kt
+++ assertk-gradle/src/main/kotlin/com/mkobit/gradle/test/assertk/gradleapi/task.kt
@@ -8,23 +8,29 @@ import org.gradle.api.Task
  * Produces an [Assert] for the description.
  * @see [Task.getDescription]
  */
-fun Assert<Task>.description(): Assert<String?> = prop(Task::getDescription)
-
-/**
- * Produces an [Assert] for the description.
- * @see [Task.getGroup]
- */
-fun Assert<Task>.group(): Assert<String?> = prop(Task::getGroup)
+fun <T : Task> Assert<T>.description(): Assert<String?> = prop(Task::getDescription)
 
 /**
  * Produces an [Assert] for if the task is enabled.
  * @see [Task.getEnabled]
  */
-fun Assert<Task>.enabled(): Assert<Boolean> = prop(Task::getEnabled)
+fun <T : Task> Assert<T>.enabled(): Assert<Boolean> = prop(Task::getEnabled)
+
+/**
+ * Produces an [Assert] for the description.
+ * @see [Task.getGroup]
+ */
+fun <T : Task> Assert<T>.group(): Assert<String?> = prop(Task::getGroup)
+
+/**
+ * Produces an [Assert] for the task NAME.
+ * @see [Task.getPath]
+ */
+fun <T : Task> Assert<T>.name(): Assert<String> = prop(Task::getName)
 
 /**
  * Produces an [Assert] for the task path.
  * @see [Task.getPath]
  */
-fun Assert<Task>.path(): Assert<String> = prop(Task::getPath)
+fun <T : Task> Assert<T>.path(): Assert<String> = prop(Task::getPath)
 
